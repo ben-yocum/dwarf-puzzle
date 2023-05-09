44f4bbc009a35f6189af16ffce71cb7998da8f40
Benjamin Yocum <benjamin.yocum@nowrx.com>
2023-05-09 11:36:31 -0700

    Various improvements

--- puzzle.js
+++ puzzle.js
@@ -18,7 +18,80 @@ const INVERTED_CELL_STATUS = Object.entries(CELL_STATUS).reduce((acc, [_, value]
   return acc;
 }, {});
 
-const data = [
+class Grid {
+
+  table = document.getElementById('table');
+
+  constructor(data) {
+    this.data = data;
+    this.height = data.length;
+    this.width = data[0].length;
+  }
+
+  drawGrid() {
+    this.removeAllChildNodes(this.table);
+
+    for (let i = 0; i < this.data.length; i++) {
+      const tr = document.createElement('tr');
+      for (let j = 0; j < this.data[i].length; j++) {
+        const td = document.createElement('td');
+        td.id = `${i}-${j}`;
+        td.style.backgroundColor = INVERTED_CELL_STATUS[this.data[i][j]].color;
+        td.onclick = (event) => this.tdClicked(event);
+        tr.appendChild(td);
+
+        if (j === this.data[i].length - 1) {
+          const td = document.createElement('td');
+          td.style.border = 'none';
+          td.id = `row-${i}`;
+          tr.appendChild(td);
+        }
+      }
+
+      document.getElementById('table').appendChild(tr);
+
+      if (i === this.data.length - 1) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < this.data[i].length; j++) {
+          const td = document.createElement('td');
+          td.style.border = 'none';
+          td.id = `col-${j}`;
+          tr.appendChild(td);
+        }
+        document.getElementById('table').appendChild(tr);
+      }
+    }
+  }
+
+  removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+
+  tdClicked(event) {
+    const [x, y] = event.target.id.split('-');
+    if (this.data[x][y] === CELL_STATUS.black.id && numAddedTiles < AVAILABLE_TILES) {
+      this.data[x][y] = CELL_STATUS.addedWhite.id;
+    }
+    this.drawGrid();
+    updateStats();
+  }
+
+  drawOutline(startX, startY, endX, endY) {
+    for (let i = startX; i <= endX; i++) {
+      document.getElementById(`${startY}-${i}`).style.borderTop = '1px solid red';
+      document.getElementById(`${endY}-${i}`).style.borderBottom = '1px solid red';
+    }
+
+    for (let i = startY; i <= endY; i++) {
+      document.getElementById(`${i}-${startX}`).style.borderLeft = '1px solid red';
+      document.getElementById(`${i}-${endX}`).style.borderRight = '1px solid red';
+    }
+  }
+}
+
+const grid = new Grid([
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
   [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0],
   [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
@@ -44,59 +117,38 @@ const data = [
   [0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1],
   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1],
   [0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0],
-];
+]);
 
-const table = document.getElementById('table');
 const AVAILABLE_TILES = 17;
 let numAddedTiles = 0;
 
-drawGrid();
+grid.drawGrid();
 updateStats();
 
-function drawGrid() {
-  removeAllChildNodes(table);
-
-  for (let i = 0; i < data.length; i++) {
-    const tr = document.createElement('tr');
-    for (let j = 0; j < data[i].length; j++) {
-      const td = document.createElement('td');
-      td.id = `${i}-${j}`;
-      td.style.backgroundColor = INVERTED_CELL_STATUS[data[i][j]].color;
-      td.onclick = tdClicked;
-      tr.appendChild(td);
-    }
-    document.getElementById('table').appendChild(tr);
-  }
-}
-
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
-function tdClicked(event) {
-  const [x, y] = event.target.id.split('-');
-  if (data[x][y] === CELL_STATUS.black.id && numAddedTiles < AVAILABLE_TILES) {
-    data[x][y] = CELL_STATUS.addedWhite.id;
-  }
-  drawGrid();
-  updateStats();
+for (let i = 0; i < grid.height-1; i+=2) {
+  grid.drawOutline(0, i, 13, i+1);
 }
 
 function updateStats() {
   let blackCount = 0;
   let whiteCount = 0;
   let addedCount = 0;
-  for (const row of data) {
-    for (const cell of row) {
+  const numWhiteInCol = new Array(grid.width).fill(0);
+  const numWhiteInRow = new Array(grid.height).fill(0);
+
+  for (let rowIdx = 0; rowIdx < grid.data.length; rowIdx++) {
+    const row = grid.data[rowIdx];
+    for (let colIdx = 0; colIdx < row.length; colIdx++) {
+      const cell = grid.data[rowIdx][colIdx];
       if (cell === CELL_STATUS.black.id) {
         blackCount++;
-      } else if (cell === CELL_STATUS.initialWhite.id) {
+      } else {
         whiteCount++;
-      } else if (cell === CELL_STATUS.addedWhite.id) {
-        whiteCount++;
-        addedCount++;
+        numWhiteInCol[colIdx]++;
+        numWhiteInRow[rowIdx]++;
+        if (cell === CELL_STATUS.addedWhite.id) {
+          addedCount++;
+        }
       }
     }
   }
@@ -105,4 +157,16 @@ function updateStats() {
   document.getElementById('total-white').innerHTML = String(whiteCount);
   document.getElementById('total-added').innerHTML = String(addedCount);
   numAddedTiles = addedCount;
+
+  for (let i = 0; i < numWhiteInCol.length; i++) {
+    document.getElementById(`col-${i}`).innerHTML = String(numWhiteInCol[i]);
+  }
+
+  for (let i = 0; i < numWhiteInRow.length; i++) {
+    document.getElementById(`row-${i}`).innerHTML = String(numWhiteInRow[i]);
+  }
+}
+
+function isWhite(id) {
+  return id === CELL_STATUS.initialWhite.id || id === CELL_STATUS.addedWhite.id;
 }
